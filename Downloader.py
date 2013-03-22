159b9a43441fbf9611c60ee3071a9556ff4eaf2e
tokuno tatsuya <tekutekutekuno@gmail.com>
2013-03-23 00:14:05 +0900

    no message

--- Downloader.py
+++ Downloader.py
@@ -35,10 +35,17 @@ def SaveImgList(imageList):
     
     for image in imageList:
         print 'get:%s' % (image)
-        output_dir = os.path.join(os.path.dirname(__file__), 'Photos')
-        savefile = os.path.join(output_dir,os.path.basename(image))
-        urllib.urlretrieve( image, savefile )
-
+        home = os.environ['HOME']
+        output_dir = os.path.join(home, 'Pictures')
+        output_dir = os.path.join(output_dir, 'Photos')
+        try:
+            savefile = os.path.join(output_dir,os.path.basename(image))
+            urllib.urlretrieve( image, savefile )
+        except:
+            os.mkdir(output_dir)
+            savefile = os.path.join(output_dir,os.path.basename(image))
+            urllib.urlretrieve( image, savefile )
+    print 'output %s' % output_dir
 
 mainList = []
 mainList.append('http://matome.naver.jp/odai/2136232246408838201/2136232643009992503')
